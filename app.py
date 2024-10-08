22b6fd56ff28c97d54a99cd25a922404e2f20b65
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2024-10-08 12:37:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-podname = os.uname()[1]
+#podname = os.uname()[1]
+
+podname = os.environ.get('POD_NAME');
+nodename = os.environ.get('NODE_NAME');
+namespace = os.environ.get('POD_NAMESPACE');
+
 
 @app.route("/")
 def index():
-    return " Container EDU | POD Working : " + podname + " | v=1\n"
+    return " Container EDU | POD Working : " + podname + " , "  | nodename : " +  nodename +  " | namespace : " +  namespace + "\n"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
